dac18924321c672bf43f2a2195fae291e8d52db2
lavinnakimberly <lavinnakimberly@gmail.com>
2017-07-21 17:59:51 -0700

    apiroutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,34 +1,49 @@
-var path = require("path");
-var dummyData = require("./../data/friends.js");
+'use strict';
 
-module.exports = function(app) {
+(function(){
+	var path = require("path");
+	var people = require("../data/friends")
 
+	module.exports = function(app){
+		app.get("/api/friends", function(req, res) {
+			res.json(people);
+		});
 
-app.get("/api/friends", function(req, res) {
+		app.post("/api/friends", function(req, res) {
+			var newFriend = req.body;
+			var currentScores = [];
+			var matchedFriend = 0;
+			var matchedScore = 100; //largest value difference
 
-    res.json(dummyData);
+			//function to add array
+			function add(a,b){
+				return a + b 
+			}
 
-});
+			//Get total score for each person in PERSON object
+			for(var i = 0; i < people.length; i++){
+				//Get scores in people.scores
+				var personScore = people[i].scores
+				var sum = personScore.reduce(add, 0);
+				currentScores.push(sum)
+			}
 
-app.post("/api/friends", function(req, res) {
-function add(a,b){
-    return a + b 
-}
-var scoreCheck = []
-for(i = 0; i < dummyData.length; i++){
-    var sum = dummyData[i].scores.reduce(add, 0);
-    scoreCheck.push(sum)
-}
+			//Get total score for new entry
+			var newFriendScore = newFriend[0].scores;
+			var newFriendSum = newFriendScore.reduce(add, 0);
 
-var newEntry = req.body.scores;
-console.log(newEntry);
-var newScore = newEntry.reduce(add,0);
-
- 
-    console.log(sum);
-    console.log(newScore);
-    res.json(req.body);
-
-});
-
-}
+			//Now, compare each score in currentScores (all of the friend's scores) with newFriendSum (the new entry's score)
+			for(var i=0; i < currentScores.length; i++){
+				//Determine difference between both people
+				var friendDelta = Math.abs((newFriendSum - currentScores[i]))
+				if(matchedScore >= friendDelta) {
+					matchedScore = friendDelta;
+					matchedFriend = i;
+				}
+			}
+			//Get name of matched person
+			var matchedPerson = people[matchedFriend];
+			res.json(matchedPerson);
+1		});
+	};
+})();
