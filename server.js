6fd456fc53883e6e210ef9160e001a0a77106e45
lavinnakimberly <lavinnakimberly@gmail.com>
2017-07-21 22:06:32 -0700

    update port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
